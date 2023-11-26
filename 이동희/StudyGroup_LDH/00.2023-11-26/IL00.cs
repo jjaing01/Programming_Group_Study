2614402ef76ac450080d2cdc9b9e52dc983143b5
DongHee Lee <jjaing01@gmail.com>
2023-11-26 11:58:09 +0900

    1126 Styudy ~ing

--- 이동희/StudyGroup_LDH/00.2023-11-26/IL00.cs
+++ 이동희/StudyGroup_LDH/00.2023-11-26/IL00.cs
@@ -1,5 +1,15 @@
 ﻿using System;
 
+/*
+ 1. IL 이란?
+자바나 C#은 컴파일 되었을 때, CPU와 OS에 독립적인 기계어 코드가 만들어지고 이것을 '중간 언어(Intermediate Language, IL)' 이라고 함
+
+이것은 CPU가 바로 해석할 수 있는게 아니라 가상 머신이 IL을 가져다가 다양한 환경 (윈도우, 리눅스,맥 등)에서 실행해준다.
+(C++ 같은 경우에도 일반 C++ 컴파일러가 아닌 .net C++ 컴파일러를 사용하면 해당 IL 코드를 만들 수 있다)
+
+이 때, 해당 가상머신의 이름을 CLR (Common Language Runtime)이라고 한다.
+*/
+
 struct Point
 {
     public int x;
@@ -11,9 +21,10 @@ class Program
     public static void Main(string[] args)
     {
         // 아래 2개의 코드의 차이점을 IL 코드로 알아보자.
-        Point pt1;
-        Point pt2 = new Point();
+        Point pt1; // 객체
+        pt1.x = 2;
+        Point pt2 = new Point(); // 객체, 모든 멤버가 초기화
 
-        Console.WriteLine($"{pt2.x}");
+        Console.WriteLine($"{pt1.x}");
     }
 }
--- 이동희/StudyGroup_LDH/00.2023-11-26/ctor00.cs
+++ 이동희/StudyGroup_LDH/00.2023-11-26/ctor00.cs
@@ -11,8 +11,6 @@ public class Point
     public Point (int x, int y)
     {
         // 1.이 부분이 주석일 경우, 쓰레기 값이 나올 것인지 0이 나올 것인지?
-        // 0이 나올 것이다. 
-        // 참조 타입의 객체를 생성하면 기본적으로 메모리를 잡고 0으로 초기화한다. 그 후 생성자가 호출된다. 
         Console.WriteLine("Point Constructor");
         this.x = x;
         this.y = y;
@@ -23,7 +21,7 @@ class Program
 {
     static void Main()
     {
-        Point pt = new Point(1, 2);
+        Point pt = new Point(); // 참조 타입은 객체를 생성하는 순간 메모리를 초기화한다.
         Console.WriteLine($"{pt.x}, {pt.y}");
     }
 }
