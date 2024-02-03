a39aaad6aca84833f1e7fe9fb62e45d3a0234b7d
ParkYongHwan <pypy8171@gmail.com>
2024-02-04 08:06:58 +0900

    2024.01.21 study

---
+++ 박용환/StudyGroup_PYH/StudyGroup_PYH/00.2024-01/21/casting.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace StudyGroup_PYH._00._2024_01_21_Casting
+{
+    class Ani
+    {
+
+    };
+
+    class Dog
+    {
+
+    }
+
+    class Point
+    {
+        public int x;
+        public int y;
+
+        public Point(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
+        }
+
+        // 변환 연산자 재정의
+        // explicit 키워드를 꼭 표기해야 한다.
+        public static explicit operator int(Point pt) { return pt.x; }
+        public static explicit operator Point(int x) { return new Point(x, x); }
+    }
+
+    // [캐스팅 규칙]
+    // 데이터 손실이 발생하지 않는 경우 -> 암시적 형변환 가능
+    // 데이터 손실이 발생하는 경우 -> 항상 명시적 캐스팅을 사용해야 한다.
+    class casting
+    {
+        public static void Foo(Animal ani)
+        {
+            // ani.Cry() // error -> Animal.Cry() 없다.
+
+            // 0. Animal 참조 타입을 Dog 참조 타입으로 캐스팅
+            Dog d = (Dog)ani;
+            d.Cry(); // Foo(new Animal()); 일 경우 예오 ㅣ발생, try ~ catch로 잡아야 한다.
+
+            // 1. 타입 조사하기
+            // (1) is : 참조 변수가 가르키는 실제 타입을 조사한다.
+            if(ani is Dog)
+            {
+                Console.WriteLine("dog");
+                Dog d1 = (Dog)ani;
+                d1.Cry();
+            }
+
+
+            // 간략화
+            if(ani is Dog dog)
+            {
+                Console.WriteLine("Dog");
+                dog.Cry();
+            }
+
+            // (2) as : 조사한 타입과 맞지 않을 경우 null 반환
+            Dog d2 = ani as Dog;
+            if(d2 == null)
+            {
+                Console.WriteLine("dog null");
+            }
+
+            // 간략화
+            Dog d3 = ani as Dog ?? new Dog();
+        }
+        public static void main()
+        {
+            int n = 3;
+            double d = 3.4;
+
+            d = n; // 암시적 가능
+            n = d; // 명시적 필요
+
+            Foo(new Dog());
+            Foo(new Animal());
+
+            // 3. 값 타입의 as 사용
+            int num = 3;
+            object obj = n;
+
+            int num1 = obj as int; // int는 null을 값으로 받을 수 없다.
+            int? num2 = obj as int?;
+
+            // 4. 변환 연산자
+            Point pt = new Point(10, 20);
+            int number = pt; // 에러 
+            int number2 = (int)pt; // OK
+
+            Point pt2 = (Point)number;
+
+            // 4-1 주의점
+            Point p3 = number2 as Point; // error (Point)number2  
+        }
+    }
+}
---
+++ 박용환/StudyGroup_PYH/StudyGroup_PYH/00.2024-01/ValueRef.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyGroup_PYH._00._2024_01
+{
+    class ValueRef
+    {
+    }
+}
+
+
+// C++과의 차이점
+// C++ 의 pointer/reference 와 C#의 reference 와는 다르다. C++ 에서는 무조건 value 형태이고, 부수적으로 pointer/reference 형으로 생성 하는 것이지만, C# 에서는 reference 형이면, 무조건 reference형으로 생성해야 하기 때문이다.
+
+// 구분
+// 대체로 reference 로 쓰고, 다음의 경우에 만족할 경우 Value 사용
+// 1. 단순이 저장용도로 쓸 것이면 value 로 사용
+// 2. 모든 public interface가 단지 내부값 확인하는데 쓰인다면, value로 사용
+// 3. 타입이 앞으로 상속될 가능성이 없다면, value를 사용
+// 4. 타입이 앞으로 다형적으로 사용 될 필요가 없다면, value를 사용
+// 5. 타입에 행동이 있어야 한다면, reference로 정의 해서 사용
+
+
+// 클래스 내부 객체에 대한 reference 반환을 피하라
+// value 타입을 이용하기
+// immutable 타입 사용하기
+// 읽기용 interface 이용하기
+// 읽기용 wapper 이용하기
