2dfa10912febb6e27dda9acce2cea0dc922d4f30
ParkYongHwan <pypy8171@gmail.com>
2024-03-03 11:33:49 +0900

    2024.02.25 study
    delegate, event, generic, indexer

---
+++ 박용환/StudyGroup_PYH/StudyGroup_PYH/2024-02/delegate.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// using FUNC2 = Func<int>;
+
+//class FUNC : System.MulticastDelegate
+//{
+//}
+
+namespace StudyGroup_PYH._2024_02
+{
+    delegate void FUNC(int arg);
+    delegate int FUNC2(int arg1, int arg2);
+
+    class Test
+    {
+        public static void static_method(int arg) { }
+        public void instance_method(int arg) { }
+
+    }
+    class @delegate
+    {
+        public static void static_method(int arg) { }
+        public void instance_method(int arg) { }
+        public static void main()
+        {
+            int n = 10;
+            // 1. 함수를 담는 데이터 타입
+            // - 메소드(메소드의 호출 정보, 메소드 모양/주소)를 저장하는 타입 : delegate
+            // - c/c++ 함수 포인터 개념과 유사하다. 하지만 함수 포인터는 주소마나 보관하지만, // malloc new 차이와 비슷해보인다
+            // - delegate는 파라미터 개수, 각각의 타입 등의 정보를 보관하고 있어서 유연성이 높다.
+
+            FUNC f = foo;
+            f.Invoke(10);
+
+            // system.MulticastDelegate
+
+            // 1-1. 실제 표기법
+            // delegate가 결국 class 이므로
+            // 결국 delegate는 메소드의 호출 적보를 저장하는 타입
+            FUNC f1 = new FUNC(foo);
+
+            // 2. delegate에 메소드를 등록하는 방법 - static인지 아닌지에 따라서 다르다.
+            FUNC f2 = @delegate.static_method;
+            FUNC f3 = Test.static_method;
+
+            // 3. instance method
+            // 객체가 존재해야 한다.
+            Test t1 = new();
+            FUNC f4 = t1.instance_method;
+
+            @delegate d1 = new();
+            FUNC f5 = d1.instance_method;
+
+            // 4. 익명 메서드
+            // delegate 등록해보자
+            // 함수 이름 대신 delegate 기입한다. -> 메소드 이름은 없다.
+            FUNC2 f6 = delegate (int a, int b) { return a * b; };
+        }
+
+        public static void foo(int arg)
+        {
+            Console.WriteLine($"foo : {arg}");
+        }
+
+        public static void foo2(FUNC2 _func2)
+        {
+            Console.WriteLine(_func2(2,2));
+        }
+    }
+}
---
+++ 박용환/StudyGroup_PYH/StudyGroup_PYH/2024-02/event.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyGroup_PYH._2024_02
+{
+    delegate void HANDLER();
+    class Button
+    {
+        public HANDLER handler_test = null;
+
+        // 1.event
+        // +=, -= 연산자만 사용할 수 있고, = 연산자는 사용할 수 없다.
+        // 이전에 등록한 것을 삭제할 수 없도록 한다.
+        // event는 구독자 간의 독립성을 위한 것이다.
+        public event HANDLER handler_event = null;
+        public void Press()
+        {
+            // 값의 존재 여부를 확인할 수 없다.
+            handler_test();
+            handler_event?.Invoke(); // elvis operator
+        }
+
+    }
+    // 2. event를 사용하는 순간 내부에서는 아래 class 형태로 변환해준다.
+    class Button_Event
+    {
+        public event HANDLER handler_event = null;
+        public void add_handler(HANDLER f) { handler_event += f; }
+        public void remove_handler(HANDLER f) { handler_event -= f; }
+        public void press()
+        {
+            handler_event?.Invoke();
+        }
+    }
+
+    class @event
+    {
+        public static void main()
+        {
+            Button btn = new Button();
+
+            btn.handler_test = F1; // A라는 사람이 등록했음
+            btn.handler_test += F2; // B라는 사람이 등록했음
+            btn.handler_test = F3; // C라는 사람이 실수로 F3 대입을 해버렸음 => 앞에 사용자들이 등록했던 기록들이 사라진다.
+            btn.Press();
+
+            // 위 실수를 방지하기 위해 event를 사용한다.
+            btn.handler_event += F1; // A라는 사람이 등록했음
+            btn.handler_event += F2; // B라는 사람이 등록했음
+            btn.handler_event += F3; // C라는 사람이 실수로 F3 대입을 해버렸음 => 앞에 사용자들이 등록했던 기록들이 사라진다.
+            btn.Press();
+        }
+        public static void F1() { Console.WriteLine("F1"); }
+        public static void F2() { Console.WriteLine("F2"); }
+        public static void F3() { Console.WriteLine("F3"); }
+    }
+}
