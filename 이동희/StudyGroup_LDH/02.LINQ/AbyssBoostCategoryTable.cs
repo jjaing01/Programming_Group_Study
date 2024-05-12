086d56ed347daab508b9a20484b8a9c5b7ce9b4e
LeeDonghee <jjaing01@naver.com>
2024-05-12 12:55:51 +0900

    AbyssBoostTable, Data
    Linq 연습용 테이블 및 데이터
    - 실제 회사에서 사용하는 디자인데이터 스키마 카피

---
+++ 이동희/StudyGroup_LDH/02.LINQ/AbyssBoostCategoryTable.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace testLinq;
+
+/// <summary>
+/// 심연 증폭 카테고리 데이터입니다.
+/// </summary>
+public partial class AbyssBoostCategoryData
+{
+    /// <summary>
+    /// 심연 증폭 카테고리 ID 입니다.
+    /// </summary>
+    public int Uid { get; set; }
+
+    /// <summary>
+    /// 심연 증폭 카테고리 해금 심연 레벨입니다.
+    /// /// </summary>
+    public int UnlockAbyssLevel { get; set; }
+
+    /// <summary>
+    /// 심연 증폭 카테고리의 직업 정보입니다.
+    /// 직업과 관련있는 카테고리를 구분하는 용도입니다.
+    /// </summary>
+    public ClassType Class { get; set; }
+}
+
+/// <summary>
+/// 심연 증폭 카테고리 테이블입니다.
+/// </summary>
+public partial class AbyssBoostCategoryTable
+{
+    /// <summary>
+    /// 심연 증폭 카테고리 데이터 목록입니다.
+    /// </summary>
+    public Dictionary<int /* AbyssBoostCategoryUid */, AbyssBoostCategoryData> dict { get; set; }
+}
