0d48acb3f1aed947de311ea565f270ce227a6dbe
sinyoung heo <hsykpu@gmail.com>
2024-06-09 13:05:33 +0900

    이전 스터디

---
+++ 허신영/StudyGroup_HSY/StudyGroup_HSY/02.LINQ/ClassType.cs
@@ -0,0 +1,8 @@
+﻿namespace testLinq;
+public enum ClassType
+{
+    None = 0,
+    Warrior = 1,
+    Wizard = 2,
+    Healer =3,
+}
---
+++ 허신영/StudyGroup_HSY/StudyGroup_HSY/02.LINQ/Master.cs
@@ -0,0 +1,15 @@
+﻿namespace testLinq;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Master
+{
+    public static void Main(string[] args)
+    {
+        return;
+    }
+}
