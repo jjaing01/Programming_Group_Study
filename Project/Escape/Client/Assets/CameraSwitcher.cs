06b1a66ae0756046aab53432fc31326a215375bf
sinyoung-heo <hsykpu@gmail.com>
2024-06-18 00:23:11 +0900

    Tab 키 입력을 통한 카메라 전환을 델리게이트를 통해 구현

--- Project/Escape/Client/Assets/CameraSwitcher.cs
+++ Project/Escape/Client/Assets/CameraSwitcher.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class CameraSwitcher : MonoBehaviour
 {
+    public Camera mainCamera;
+    public Camera debugCamera;
+
+    public delegate void OnCameraSwitch();
+    public static event OnCameraSwitch CameraSwitchEvent;
+
+    private bool isDebugCameraActive = false;
+
     // Start is called before the first frame update
     void Start()
     {
         // Ensure only the main camera is active at the start
         mainCamera.enabled = true;
         debugCamera.enabled = false;
+
+        CameraSwitchEvent += SwitchCameras;
     }
 
     // Update is called once per frame
@@ -18,24 +28,34 @@ public class CameraSwitcher : MonoBehaviour
         // Switch cameras when the Tab key is pressed
         if (Input.GetKeyDown(KeyCode.Tab))
         {
-            mainCamera.enabled = !mainCamera.enabled;
-            debugCamera.enabled = !debugCamera.enabled;
-
-            // Lock and hide the cursor when the debug camera is active
-            // , and unlock and show it when the main camera is active
-            if (debugCamera.enabled)
-            {
-                Cursor.lockState = CursorLockMode.Locked;
-                Cursor.visible = false;
-            }
-            else
-            {
-                Cursor.lockState = CursorLockMode.None;
-                Cursor.visible = true;
-            }
+            CameraSwitchEvent?.Invoke();
         }
     }
 
-    public Camera mainCamera;
-    public Camera debugCamera;
+    private void SwitchCameras()
+    {
+        // Toggle the Camera activation states
+        isDebugCameraActive = !(isDebugCameraActive);
+
+        mainCamera.enabled = !(isDebugCameraActive);
+        debugCamera.enabled = isDebugCameraActive;
+
+        // Lock and hide the cursor when the debug camera is active
+        // , and unlock and show it when the main camera is active
+        if (isDebugCameraActive)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        CameraSwitchEvent -= SwitchCameras;
+    }
 }
