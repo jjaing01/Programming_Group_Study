76c022fc5be86907b9af1eac2b0b7e4399d63b9d
sinyoung heo <hsykpu@gmail.com>
2024-09-17 16:22:07 +0900

    https://gesc.atlassian.net/browse/ESCTASK-28
    엑셀 테이블 기본 정보 파싱

---
+++ Project/Escape/Shared/ExcelParsher/ExcelParsher/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelParsher
+{
+    public static class Constants
+    {
+        public const int DataTypeRow = 0;
+        public const int DataNameRow = 1;
+
+        public const string TableDirPath = @"..\..\..\..\ExcelDataTable";
+    }
+}
---
+++ Project/Escape/Shared/ExcelParsher/ExcelParsher/TableCommonInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelParsher
+{
+    class TableCommonInfo
+    {
+        public List<string> dataTypes;
+        public List<string> dataNames;
+    }
+}
