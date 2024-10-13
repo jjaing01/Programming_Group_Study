3deaf3ddf2854e1aaab161bc8775658abe3a5a3b
sinyoung-heo <hsykpu@gmail.com>
2024-10-14 01:00:20 +0900

    https://gesc.atlassian.net/browse/ESCTASK-28 #cs 파일 생성기 클래스 추가 #using 구문 file write 추가

---
+++ Project/Escape/Shared/ExcelParsher/ExcelParsher/CSFileGenerator.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ExcelParsher
+{
+    internal class CSFileGenerator
+    {
+        private static CSFileGenerator instance = null;
+        private static readonly object _lock = new object();
+
+        public static CSFileGenerator GetInstance()
+        {
+            if (instance is null)
+            {
+                lock (_lock)
+                {
+                    instance = new CSFileGenerator();
+                }
+            }
+
+            return instance;
+        }
+
+        public void MakeDataTableFile(string fileName)
+        {
+            string path = Constants.TableDirPath + @"\" + fileName + @".cs";
+            string contents = "";
+
+            contents = WriteUsing(contents);
+
+            File.WriteAllText(path, contents);
+        }
+
+        public string WriteUsing(string contents)
+        {
+            string output = "";
+
+            output += "using System;\n";
+            output += "using System.Collections.Generic;\n";
+            output += "using System.Linq;\n";
+            output += "using System.Text;\n";
+            output += "using System.Threading.Tasks;\n";
+            output += "using System.IO;\n";
+
+            return contents + output;
+        }
+    }
+}
--- Project/Escape/Shared/ExcelParsher/ExcelParsher/NPOIAdapter.cs
+++ Project/Escape/Shared/ExcelParsher/ExcelParsher/NPOIAdapter.cs
@@ -15,9 +15,9 @@ namespace ExcelParsher
     internal class NPOIAdapter : ExcelAdapter
     {
         private Dictionary<string, IWorkbook> workbooks;
-
         private static NPOIAdapter instance = null;
         private static readonly object _lock = new object();
+
         public static NPOIAdapter GetInstance()
         {
             if (instance is null)
@@ -70,7 +70,7 @@ namespace ExcelParsher
 
                     string keyName = workbook.Key + @"+" + sheet.SheetName;
                     AddSheetInfo(keyName, info);
-                    MakeDataTableFile(workbook.Key + sheet.SheetName);
+                    CSFileGenerator.GetInstance().MakeDataTableFile(workbook.Key + sheet.SheetName);
 
                     //// 첫 번째 행 (헤더) 건너뛰기 위해 1부터 시작
                     //for (int row = 2; row <= sheet.LastRowNum; row++)
@@ -113,12 +113,6 @@ namespace ExcelParsher
             }
 
             return types;
-        }
-
-        private void MakeDataTableFile(string fileName)
-        {
-            string path = Constants.TableDirPath + @"\" + fileName + @".cs";
-            File.WriteAllText(path, "test file");
         }
     }
 }
