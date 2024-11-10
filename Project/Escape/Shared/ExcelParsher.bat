a30f18cd9e79c7bfc4f23c611ab647e7e655279e
sinyoung-heo <hsykpu@gmail.com>
2024-11-11 01:10:08 +0900

    https://gesc.atlassian.net/browse/ESCTASK-30
    복사 배치파일 수정

--- Project/Escape/Shared/ExcelParsher.bat
+++ Project/Escape/Shared/ExcelParsher.bat
@@ -1,59 +1,12 @@
 @echo off
-setlocal enabledelayedexpansion
 
-REM 현재 디렉토리 저장
-set "ORIGINAL_DIR=%CD%"
+cd ExcelParsher\ExcelParsher\bin\Release
+ExcelParsher.exe
 
-REM ExcelParsher.exe 실행
-echo Running ExcelParsher.exe...
-cd /d "%~dp0ExcelParsher\ExcelParsher\bin\Release"
-if exist "ExcelParsher.exe" (
-    ExcelParsher.exe
-    if !ERRORLEVEL! neq 0 (
-        echo Error: ExcelParsher.exe failed to execute. Error code: !ERRORLEVEL!
-        goto :error
-    )
-) else (
-    echo Error: ExcelParsher.exe not found in the current directory.
-    goto :error
-)
+cd ..\..\..\..\
+cd ExcelDataTable
 
-REM 원래 디렉토리로 돌아가기
-cd /d "%ORIGINAL_DIR%"
+xcopy /y *.xlsx ..\..\Client\Assets\ExcelDataTable
+xcopy /y *.cs ..\..\Client\Assets\ExcelParsher
 
-REM .cs 파일 복사
-echo Copying .cs files...
-if exist "ExcelDataTable" (
-    if not exist "..\Client\Assets\ExcelParsher" mkdir "..\Client\Assets\ExcelParsher"
-    xcopy "ExcelDataTable\*.cs" "..\Client\Assets\ExcelParsher" /Y /I
-    if !ERRORLEVEL! neq 0 goto :copy_error
-) else (
-    echo Error: ExcelDataTable directory not found.
-    goto :error
-)
-
-REM .xlsx 파일 복사
-echo Copying .xlsx files...
-if exist "ExcelDataTable" (
-    if not exist "..\Client\Assets\ExcelDataTable" mkdir "..\Client\Assets\ExcelDataTable"
-    xcopy "ExcelDataTable\*.xlsx" "..\Client\Assets\ExcelDataTable" /Y /I
-    if !ERRORLEVEL! neq 0 goto :copy_error
-) else (
-    echo Error: ExcelDataTable directory not found.
-    goto :error
-)
-
-echo All operations completed successfully.
-goto :eof
-
-:copy_error
-echo An error occurred during file copying. Error level: !ERRORLEVEL!
-goto :error
-
-:error
-echo An error occurred. Exiting with error level !ERRORLEVEL!.
 pause
-exit /b !ERRORLEVEL!
-
-:eof
-endlocal
